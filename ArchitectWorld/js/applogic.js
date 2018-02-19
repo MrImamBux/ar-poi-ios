23a60fb6835011311d8a5c89b38e1b0c5af41f2a
Farrukh Ahmed <farukhcs15@gmail.com>
2018-02-19 18:52:53 +0100

    Geo Pointers Data

---
+++ ArchitectWorld/js/applogic.js
@@ -0,0 +1,183 @@
+/**
+ * IMAM BUX-FARRUKH-TAIMOOR
+ * @type {{initiallyLoadedData: boolean, pointerDrawable_idle: null, pointerDrawable_selected: null, pointerDrawable_directionIndicator: null, pointerList: Array,
+ * currentPointer: null, loadPointersFromJsonData: Function, preCalculatePointersDistance: Function, updateStatusMessage: Function, locationChanged: Function,
+ * onPointerSelected: Function, onScreenClick: Function, showDetailsInAndroidActivity: Function, loadDataFromDataFile: Function, showLoader: Function,
+ * hideLoader: Function, onBuildingClickFromList: Function}}
+ */
+var World = {
+
+	initiallyLoadedData: false,
+
+	// UI Assets for Pointers
+	pointerDrawable_idle: null,
+	pointerDrawable_selected: null,
+	pointerDrawable_directionIndicator: null,
+
+	// list of AR.GeoObjects that are visible on screen
+	pointerList: [],
+
+	// selected pointer
+	currentPointer: null,
+
+	loadPointersFromJsonData: function loadPoisFromJsonDataFn(poiData) {
+
+		World.pointerList = [];
+
+		World.pointerDrawable_idle = new AR.ImageResource("assets/pointer_idle.png");
+		World.pointerDrawable_selected = new AR.ImageResource("assets/pointer_selected.png");
+		World.pointerDrawable_directionIndicator = new AR.ImageResource("assets/indi.png");
+
+		for (var i = 0; i < poiData.length; i++) {
+			var singlePointer = {
+				"id": poiData[i].id,
+				"latitude": parseFloat(poiData[i].latitude),
+				"longitude": parseFloat(poiData[i].longitude),
+				"altitude": AR.CONST.UNKNOWN_ALTITUDE,
+				"title": poiData[i].name,
+				"description": poiData[i].description,
+                "buildingName": poiData[i].buildingName,
+                "thumbnailURL": poiData[i].thumbnailURL,
+                "buildingImages": poiData[i].buildingImages
+			};
+
+			World.pointerList.push(new Pointer(singlePointer));
+		}
+
+        // updates distance information of all places
+        World.preCalculatePointersDistance();
+
+		World.updateStatusMessage('App currently supports ' + i + ' buildings');
+	},
+
+    // calculating distance while loading pointers
+    preCalculatePointersDistance: function preCalculatePointersDistanceFn() {
+        for (var i = 0; i < World.pointerList.length; i++) {
+            World.pointerList[i].distanceToUser = World.pointerList[i].pointerObject.locations[0].distanceToUser();
+        }
+    },
+
+    // shows the message once all the data is loaded
+	updateStatusMessage: function updateStatusMessageFn(message, isWarning) {
+
+		var themeToUse = isWarning ? "e" : "c";
+		var iconToUse = isWarning ? "alert" : "info";
+
+		$("#status-message").html(message);
+		$("#popupInfoButton").buttonMarkup({
+			theme: themeToUse
+		});
+		$("#popupInfoButton").buttonMarkup({
+			icon: iconToUse
+		});
+	},
+
+	// location updates, fired every time you call architectView.setLocation() in native environment
+	locationChanged: function locationChangedFn(lat, lon, alt, acc) {
+
+		if (!World.initiallyLoadedData) {
+			World.loadDataFromDataFile(lat, lon, alt, acc);
+			World.initiallyLoadedData = true;
+		}
+	},
+
+	onPointerSelected: function onPointerSelectedFn(pointer) {
+		World.currentPointer = pointer;
+
+        // update panel values
+        $("#buildingName").html(pointer.ptrCoordinates.buildingName);
+        $("#pointerDescription").html(pointer.ptrCoordinates.description);
+
+        // in-case distance wasn't calculated properly, re-calculate it
+        if( undefined == pointer.distanceToUser ) {
+            pointer.distanceToUser = pointer.pointerObject.locations[0].distanceToUser();
+        }
+
+        // calculating the unit of distance from user location to the pointer
+        var distanceWithUnit = (pointer.distanceToUser > 999) ? ((pointer.distanceToUser / 1000).toFixed(2) + " km") : (Math.round(pointer.distanceToUser) + " m");
+        $("#pointerDistance").html(distanceWithUnit);
+
+        // show panel
+        $("#pointerDataPanel").panel("open");
+
+        // using jquery mobile 'panelbeforeclose' call back to deselect the selected pointer as well while closing panel
+        $("#pointerDataPanel").on("panelbeforeclose", function(event, ui) {
+            World.currentPointer.setDeselected(World.currentPointer);
+        });
+
+	},
+
+	// screen was clicked but no geo-object was hit
+	onScreenClick: function onScreenClickFn() {
+		if (World.currentPointer) {
+			World.currentPointer.setDeselected(World.currentPointer);
+		}
+	},
+
+    showDetailsInAndroidActivity: function showDetailsInAndroidActivityFn() {
+
+        var currentPointer = World.currentPointer;
+        var pointerSelectedJSON = {
+            action: "pointer_detailed_activity",
+            id: currentPointer.ptrCoordinates.id,
+            title: currentPointer.ptrCoordinates.title,
+            description: currentPointer.ptrCoordinates.description
+        };
+        /*
+         The sendJSONObject method can be used to send data from javascript to the native code.
+         */
+        AR.platform.sendJSONObject(pointerSelectedJSON);
+
+    },
+
+	// request data
+	loadDataFromDataFile: function loadDataFromDataFileFn(lat, lon, alt, acc) {
+		World.loadPointersFromJsonData(pointersData);
+
+        /*******************
+         * Configure assets
+         *******************/
+        // show radar & set click-listener
+        PointersRadar.show();
+        $('#radarDiv').unbind('click');
+        $("#radarDiv").click(PointersRadar.clickedRadar);
+
+        //configuring photo gallery and click listener
+        ImageGallery.init();
+        ImgTracking.init();
+        ImgTrackingHS.init();
+
+	},
+
+    showLoader : function showLoaderFn() {
+
+        var $this = $( this ),
+            theme = $this.jqmData( "theme" ) || $.mobile.loader.prototype.options.theme,
+            msgText = $this.jqmData( "msgtext" ) || $.mobile.loader.prototype.options.text,
+            textVisible = $this.jqmData( "textvisible" ) || $.mobile.loader.prototype.options.textVisible,
+            textonly = !!$this.jqmData( "textonly" );
+        html = $this.jqmData( "html" ) || "";
+        $.mobile.loading( 'show', {
+            text: msgText,
+            textVisible: textVisible,
+            theme: theme,
+            textonly: textonly,
+            html: html
+        });
+    },
+
+    hideLoader : function hideLoaderFn() {
+        $.mobile.loading( "hide" );
+    },
+
+    //when a user click on a building in all buildings list view panel at the left of screen
+    onBuildingClickFromList: function onBuildingClickFromListFn(pointer) {
+        World.onPointerSelected(pointer);
+    }
+
+};
+
+AR.context.onLocationChanged = World.locationChanged;
+
+//inactivate the pointer if it is in selected state and user taps on the screen
+AR.context.onScreenClick = World.onScreenClick;
---
+++ ArchitectWorld/js/data.js
@@ -0,0 +1,372 @@
+
+var pointersData = [
+    {
+        "id": 2,
+        "longitude": 9.685557,
+        "latitude": 50.562698,
+        "altitude": 298,
+        "name": "H1",
+        "description": "Bus Stop towards ZOB. Bus # 06 comes here every 30 minutes towards the direction of ZOB / Fulda Bahnhof",
+        "buildingName": "BusStop > ZOB",
+        "thumbnailURL": "assets/gallery/hs_fulda.jpg"
+    },
+    {
+        "id": 1,
+        "longitude": 9.686970,
+        "latitude": 50.563331,
+        "altitude": 294,
+        "name": "H2",
+        "description": "Bus Stop towards Hochschule. Bus # 06 comes here every 30 minutes towards the direction of Hochschule.",
+        "buildingName": "Bus Stop > HS",
+        "thumbnailURL": "assets/gallery/hs_fulda.jpg"
+    },
+    {
+        "id": 3,
+        "longitude": 9.685239,
+        "latitude": 50.564062,
+        "altitude": 300,
+        "name": "C (43)",
+        "description": "Offices of the concerned persons for international student's consultation specially M.Sc. GSD group. Advisers such as Ms. Hirth, Mrs. Beate etc. sits here. Also issues related to System2Teach could be solved by visiting Room # 07.",
+        "buildingName": "Building # 43 (C)",
+        "thumbnailURL": "assets/gallery/C/1-min.jpg",
+        "buildingImages": [
+            {
+                "src": "assets/gallery/C/1-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 43 (C)'
+            },
+            {
+                "src": "assets/gallery/C/2-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 43 (C)'
+            },
+            {
+                "src": "assets/gallery/C/3-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 43 (C)'
+            },
+            {
+                "src": "assets/gallery/C/4-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 43 (C)'
+            },
+            {
+                "src": "assets/gallery/C/5-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 43 (C)'
+            },
+            {
+                "src": "assets/gallery/C/6-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 43 (C)'
+            }
+        ]
+    },
+    {
+        "id": 4,
+        "longitude": 9.686009,
+        "latitude": 50.564914,
+        "altitude": 302,
+        "name": "E (46)",
+        "buildingName": "Building # 46 (E)",
+        "description": "Main building for Department of Computer Science. All labs such as Linux, Cloud Computing or IOS are here. Major CS classes also held in this same building.",
+        "thumbnailURL": "assets/gallery/E/4-min.jpg",
+        "buildingImages": [
+            {
+                "src": "assets/gallery/E/1-min.jpg",
+                "w": 1080,
+                "h": 1080,
+                "title": 'Building # 46 (E)'
+            },
+            {
+                "src": "assets/gallery/E/2-min.jpg",
+                "w": 1080,
+                "h": 1080,
+                "title": 'Building # 46 (E)'
+            },
+            {
+                "src": "assets/gallery/E/3-min.jpg",
+                "w": 1080,
+                "h": 1080,
+                "title": 'Building # 46 (E)'
+            },
+            {
+                "src": "assets/gallery/E/4-min.jpg",
+                "w": 1024,
+                "h": 683,
+                "title": 'Building # 46 (E)'
+            },
+            {
+                "src": "assets/gallery/E/5-min.jpg",
+                "w": 1024,
+                "h": 683,
+                "title": 'Building # 46 (E)'
+            },
+            {
+                "src": "assets/gallery/E/6-min.jpg",
+                "w": 1024,
+                "h": 683,
+                "title": 'Building # 46 (E)'
+            },
+            {
+                "src": "assets/gallery/E/7-min.jpg",
+                "w": 1024,
+                "h": 683,
+                "title": 'Building # 46 (E)'
+            },
+            {
+                "src": "assets/gallery/E/8-min.jpg",
+                "w": 1024,
+                "h": 683,
+                "title": 'Building # 46 (E)'
+            },
+            {
+                "src": "assets/gallery/E/9-min.jpg",
+                "w": 1024,
+                "h": 683,
+                "title": 'Building # 46 (E)'
+            }
+        ]
+    },
+    {
+        "id": 5,
+        "longitude": 9.684593,
+        "latitude": 50.564979,
+        "altitude": 300,
+        "name": "G (51)",
+        "buildingName": "Building # 51 (G)",
+        "description": "You can find offices of the Professors here (Prof. Todenhöfer, Prof. Herpers. etc). There are two computer labs with a printing room as well in this building.",
+        "thumbnailURL": "assets/gallery/G/1-min.jpg",
+        "buildingImages": [
+            {
+                "src": "assets/gallery/G/1-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 51 (G)'
+            },
+            {
+                "src": "assets/gallery/G/2-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 51 (G)'
+            },
+            {
+                "src": "assets/gallery/G/3-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 51 (G)'
+            },
+            {
+                "src": "assets/gallery/G/5-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 51 (G)'
+            }
+        ]
+    },
+    {
+        "id": 6,
+        "longitude": 9.68942642,
+        "latitude": 50.56657749,
+        "altitude": 307,
+        "name": "Q (23)",
+        "description": "Few of social sciences related classes held in this building. Beside this building you can find the offices of Studentenwerk.",
+        "buildingName": "Building # 23 (Q)",
+        "thumbnailURL": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/banners/hs_fulda.jpg"
+        //"buildingImages": [
+        //    {
+        //        "src": "https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_b.jpg",
+        //        "w": 964,
+        //        "h": 1024,
+        //        "title": 'Image1 Caption'
+        //    }
+        //]
+    },
+    {
+        "id": 7,
+        "longitude": 9.687292,
+        "latitude": 50.564968,
+        "altitude": 306,
+        "name": "U (12)",
+        "buildingName": "Building # 12 (U)",
+        "description": "Bibliothek / Library of the university. Books can be borrowed from the library using library's web portal for 5 weeks. This can be extended as well.",
+        "thumbnailURL": "assets/gallery/U/5-min.jpg",
+        "buildingImages": [
+            {
+                "src": "assets/gallery/U/1-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 12 (U)'
+            },
+            {
+                "src": "assets/gallery/U/2-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 12 (U)'
+            },
+            {
+                "src": "assets/gallery/U/3-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 12 (U)'
+            },
+            {
+                "src": "assets/gallery/U/4-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 12 (U)'
+            },
+            {
+                "src": "assets/gallery/U/5-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 12 (U)'
+            },
+            {
+                "src": "assets/gallery/U/6-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 12 (U)'
+            },
+            {
+                "src": "assets/gallery/U/7-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 12 (U)'
+            }
+        ]
+    },
+    {
+        "id": 8,
+        "longitude": 9.687285,
+        "latitude": 50.565852,
+        "altitude": 304,
+        "name": "T (11)",
+        "description": "MENSA. This is basically the place to eat. Students gets subsidy on the lunch and refreshments. You need to charge your ID CARD using the machine outside for paying. No money accepts here.",
+        "buildingName": "Building # 11 (T)",
+        "thumbnailURL": "assets/gallery/T/5-min.jpg",
+        "buildingImages": [
+            {
+                "src": "assets/gallery/T/1-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 11 (T)'
+            },
+            {
+                "src": "assets/gallery/T/2-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 11 (T)'
+            },
+            {
+                "src": "assets/gallery/T/3-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 11 (T)'
+            },
+            {
+                "src": "assets/gallery/T/4-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 11 (T)'
+            },
+            {
+                "src": "assets/gallery/T/5-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 11 (T)'
+            },
+            {
+                "src": "assets/gallery/T/6-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 11 (T)'
+            },
+            {
+                "src": "assets/gallery/T/7-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 11 (T)'
+            }
+        ]
+    },
+    {
+        "id": 9,
+        "longitude": 9.686089,
+        "latitude": 50.565859,
+        "altitude": 304,
+        "name": "S (10)",
+        "buildingName": "Building # 10 (S)",
+        "description": "SSC. International students service center where you can register yourself, get whatever information you want from front office. You can also visit to get advise from counsellors.",
+        "thumbnailURL": "assets/gallery/S/6-min.jpg",
+        "buildingImages": [
+            {
+                "src": "assets/gallery/S/1-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 10 (S)'
+            },
+            {
+                "src": "assets/gallery/S/2-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 10 (S)'
+            },
+            {
+                "src": "assets/gallery/S/3-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 10 (S)'
+            },
+            {
+                "src": "assets/gallery/S/5-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 10 (S)'
+            },
+            {
+                "src": "assets/gallery/S/6-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 10 (S)'
+            },
+            {
+                "src": "assets/gallery/S/7-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 10 (S)'
+            },
+            {
+                "src": "assets/gallery/S/8-min.jpg",
+                "w": 964,
+                "h": 1024,
+                "title": 'Building # 10 (S)'
+            }
+        ]
+    },
+    {
+        "id": 10,
+        "longitude": 9.6713135,
+        "latitude": 50.5497386,
+        "altitude": 252,
+        "name": "KL",
+        "buildingName": "Kaufland Mart",
+        "description": "Kaufland",
+        "thumbnailURL": "assets/gallery/hs_fulda.jpg"
+    },
+    {
+        "id": 11,
+        "longitude": 9.66860443,
+        "latitude": 50.54844334,
+        "altitude": 247,
+        "name": "PARK",
+        "buildingName": "Jogging Park",
+        "description": "Beautiful Park",
+        "thumbnailURL": "assets/gallery/hs_fulda.jpg"
+    }];
